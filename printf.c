ce1b4384d1c30102bf1bdf57c42dcc6b0451491c
JohnCook17 <655@holbertonschool.com>
2019-03-17 04:53:12 +0000

    printf.c, print the arguments given to it

---
+++ printf.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int j = 0;
+	va_list list;
+	form_t f[] = {
+		{"c", printc},
+		{"s", prints},
+		{"d", printd},
+		{"i", printi},
+		{"%", printp},
+		{NULL, NULL}
+	};
+
+	va_start(list, format);
+	while (format && format[j] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			while (f[j].type)
+			{
+				if (format[i + 1] == *f[j].type)
+				{
+					f[j].func(list);
+				}
+				j++;
+			}
+		}
+		else
+			_putchar(format[i]);
+		i++;
+	}
+	va_end(list);
+	return (0);
+}
