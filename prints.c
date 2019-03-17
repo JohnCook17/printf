6a2905ffe8b962d5945ac02b8dad2d09693606cb
JohnCook17 <655@holbertonschool.com>
2019-03-17 02:29:31 +0000

    prints a string

---
+++ prints.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * prints - prints a string
+ * @list: list of values to print
+ */
+void prints(va_list list)
+{
+	int i = 0;
+	char *s = va_arg(list, char *);
+
+	while (s)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
