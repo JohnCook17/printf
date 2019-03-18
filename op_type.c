c4ab4af939b4172678891552e233fc1cdf34683d
JohnCook17 <655@holbertonschool.com>
2019-03-18 20:45:13 +0000

    got the count working

--- op_type.c
+++ op_type.c
@@ -1,9 +1,8 @@
- #include "holberton.h"
+#include "holberton.h"
 #include <stdio.h>
-int op_type(const char *pointer, va_list list)
+int op_type(const char pointer, va_list list)
 {
 	int i;
-	int count = 0;
 	form_t f[] = {
 		{"c", print_c},
 		{"s", print_s},
@@ -11,9 +10,8 @@ int op_type(const char *pointer, va_list list)
 			{"i", print_i},*/
 		{NULL, NULL}
 	};
-
- 	for (i = 0; f[i].type; i++)
-		if (pointer == f[i].type)
- 			count += f[i].func(list);
-	return (count);
+ 	for (i = 0; i < 2; i++)
+		if (pointer == *f[i].type)
+			return (f[i].func(list));
+	return (0);
 }
